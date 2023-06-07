e97de34162f573fe6a920929ef59c6771300c4dd
AdrianaMeyer <adriana.meyer@mercadolivre.com>
2023-06-07 19:08:47 -0300

    Criando o controller de products com os métodos respectivos

---
+++ cmd/server/handler/products.go
@@ -0,0 +1,87 @@
+package handler
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/AdrianaMeyer/BootcampGo-API/internal/products"
+	"github.com/gin-gonic/gin"
+)
+
+type request struct {
+	Name 		string 		`json:"name"`
+	Color 		string 		`json:"color"`
+	Price 		float64 	`json:"price"`
+	Count 		int 		`json:"count"`
+	Code 		string 		`json:"code"`
+	Published 	bool 		`json:"published"`
+	Date 		time.Time 	`json:"date"`
+}
+
+type Product struct {
+	service products.IService
+
+}
+
+func NewProduct(p products.IService) *Product {
+	return &Product {
+		service: p,
+	}
+}
+
+func (c *Product) GetAll() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token := ctx.Request.Header.Get("token")
+		if token != "123456" {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"Error": "Token Inválido",
+			})
+			return
+		}
+
+		product, err := c.service.GetAll()
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"Error": err.Error(),
+			})
+		return
+		}
+
+		ctx.JSON(http.StatusOK, product)
+
+	}
+}
+
+func (c *Product) Save() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token := ctx.Request.Header.Get("token")
+		if token != "123456" {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"Error": "Token Inválido",
+			})
+			return
+		}
+
+		var req request
+		err := ctx.Bind(&req)
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"Error": err.Error(),
+			})
+			return	
+		}
+		
+		date := time.Now()
+
+		product, err := c.service.Save(req.Name, req.Color, req.Price, req.Count, req.Code, req.Published, date)
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"Error": err.Error(),
+			})
+			return	
+		}
+
+		ctx.JSON(http.StatusOK, product)
+
+	}
+}
